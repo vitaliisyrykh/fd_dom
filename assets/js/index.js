d87e6ad2092c776adea703c45acaddc314fa0e5d
Vladimir <freshcode.yt@gmail.com>
2021-01-21 16:19:35 +0200

    Add clickHandlers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-/*  */
 const [btn] = document.getElementsByTagName('button');
 
-const par = document.querySelector('section > article > p');
+function showAlert() {
+  alert('HELLO DOM!');
+}
+
+function log(){
+  console.log('event happened!')
+}
+
+/* 2 способа запуска обраточиков */
+btn.addEventListener('click', showAlert );
+btn.addEventListener('click', log);
+// btn.onclick = showAlert;
