e178c6e827dcb77374ab98d31db454ebf53f2b18
Vladimir <freshcode.yt@gmail.com>
2021-01-21 16:21:33 +0200

    Add div task solution

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const [btn] = document.getElementsByTagName('button');
+const [div] = document.getElementsByTagName('div');
 
 function showAlert() {
   alert('HELLO DOM!');
 }
 
-function log(){
-  console.log('event happened!')
+function log() {
+  console.log('event happened!');
 }
 
 /* 2 способа запуска обраточиков */
-btn.addEventListener('click', showAlert );
+btn.addEventListener('click', showAlert);
 btn.addEventListener('click', log);
 // btn.onclick = showAlert;
+div.onclick = log;
+// div.addEventListener('click', log)
