993a82b9491990404a99dc2c5d1c9ef9436a0c98
Vladimir <freshcode.yt@gmail.com>
2021-01-25 15:57:04 +0200

    Add data-*

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,16 @@
 'use strict';
 
-const [closeBtn, openBtn] = document.querySelectorAll('button');
+const [firstBtn, secondBtn] = document.querySelectorAll('button');
 
-const clickHandler = ({ target }) => {
-  alert(target.textContent);
+const clickHandler = (event) => {
+  const {
+    target: {
+      dataset,
+    },
+  } = event;
+
+  console.log(dataset);
 };
 
-closeBtn.addEventListener('click', clickHandler);
-openBtn.addEventListener('click', clickHandler);
-
-/* 
-Поменять местами содержимое кнопок 
-по наведению на кнопку close 
-*/
-
-const switchHandler = ({ target }) => {
-  const closeElement = target === closeBtn ? closeBtn : openBtn;
-  const openElement = target === closeBtn ? openBtn : closeBtn;
-
-  const closeText = closeElement.textContent;
-  closeElement.textContent = openElement.textContent;
-  openElement.textContent = closeText;
-
-  closeElement.removeEventListener('mouseenter', switchHandler);
-  openElement.addEventListener('mouseenter', switchHandler);
-};
-
-closeBtn.addEventListener('mouseenter', switchHandler);
+firstBtn.addEventListener('click', clickHandler);
+secondBtn.addEventListener('click', clickHandler);
