3de0eb8258a3d7b5f9ffc6a5813d6f72c05852ef
Vladimir <freshcode.yt@gmail.com>
2021-01-27 14:43:04 +0200

    Add bind example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* 
 const btn = document.querySelector('button');
 const div = document.querySelector('#root');
 
@@ -15,3 +15,30 @@ document.body.addEventListener('click', clickHandler);
 window.addEventListener('click', clickHandler, {
   capture: true,
 });
+ */
+
+const sumContext = {
+  a: 10,
+  b: 20,
+  c: 30,
+};
+
+function sum(...args) {
+  console.log(this);
+}
+sum(123, 123, 123, 123, 123, 1234);
+
+const sumWithThis = sum.bind(sumContext);
+
+sumWithThis();
+
+const data = [1, 2, 3];
+function test() {
+  console.log(this)
+}
+
+test();
+
+const newTestFuncWithContext = test.bind(window);
+
+data.forEach(newTestFuncWithContext);
---
+++ assets/js/sandbox.js
@@ -0,0 +1,9 @@
+const institut = { name: 'ZNTu' };
+
+const student = { name: 'test' };
+
+student.institut = institut;
+
+console.log(student);
+
+
