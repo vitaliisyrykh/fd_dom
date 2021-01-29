1acb001a4fafd6114d203eae118dac165811aecb
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-01-29 16:59:47 +0200

    add createElement

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -8,11 +8,36 @@ const cardContainer = document.getElementById('root');
 const cards = responseData.map((user)=>createUserCardElement(user));
 
 function createUserCardElement(user){
-  const {discription, firstName, id, lastName}=user;
+  const {discription, firstName, lastName,}=user;
  
   const p = createElement('p',{ classNames:['cardDescription']},[document.createTextNode(discription)]);
   const h3 = createElement('h3',{classNames:['cardFirstName']},document.createTextNode(firstName));
-  const h4 = createElement('h4',{classNames:['cardSurName']}, document.createTextNode(surName));
+  const h4 = createElement('h4',{classNames:['cardLastName']}, document.createTextNode(lastName));
+  const divIcons = createElement('div',{classNames:['iconsSocialWrapper']});
+  const img = createImg(user);
+  const socialIcons = createIcons(user);
+
+
+
 };
 
+function createCardImage(user){
+  const {firstName, id}=user;
+  
+}
 
+/**
+ *
+ * @param {string} type
+ * @param {object} options
+ * @param {string[]} options.classNames
+ * @param {function} options.onClick
+ * @param {HTMLElement[]} children
+ */
+function createElement(type, { classNames, onClick }, children) {
+  const elem = document.createElement(type);
+  elem.classList.add(...classNames);
+  elem.onclick = onClick;
+  elem.append(...children);
+  return elem;
+}
