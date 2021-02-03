8316a4e136724d8860f29fad05755c761dfbc437
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-02-03 21:35:56 +0200

    delete coment

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,7 +1,5 @@
 "use strict";
-/* new URL('https://www.facebook.com/DwayneJohnson'); // {hostname}
-new Map().set('www.facebook.com', 'src to fb icon'); // key - hostname
- */
+
 const supportedSocialNetworks = new Map();
 supportedSocialNetworks.set("www.facebook.com", "./assets/icons/facebook.svg");
 supportedSocialNetworks.set("twitter.com", "./assets/icons/instagram.svg");
@@ -12,13 +10,4 @@ const cardContainer = document.getElementById("root");
 const cards = responseData.map((user) => createUserCardElement(user));
 cardContainer.append(...cards);
 
-
-
 const [icons] = responseData.map((user) => createIcons(user.contacts));
-
-
-
-
-
-
-
