edc7c904cb3a59e9e4c479083f2428d4d181d7d4
Vitalii Syrykh <vitaliysyryck@gmail.com>
2021-02-03 23:02:36 +0200

    add elementNameWrapper classNameNameMArgin

--- assets/js/rander_cards/create_user_card_elem.js
+++ assets/js/rander_cards/create_user_card_elem.js
@@ -4,20 +4,20 @@ function createUserCardElement(user) {
   const p = createElement("p", { classNames: ["cardDescription"] }, [
     document.createTextNode(discription),
   ]);
-  const h3 = createElement("h3", { classNames: ["cardFirstName"] }, [
+  const h3 = createElement("h3", { classNames: ["cardFirstName", "nameMargin"] }, [
     document.createTextNode(firstName),
   ]);
-  const h4 = createElement("h4", { classNames: ["cardLastName"] }, [
+  const h4 = createElement("h4", { classNames: ["cardLastName","nameMargin"] }, [
     document.createTextNode(lastName),
   ]);
+  const nameWrapper = createElement('div',{classNames:['nameWrapper']},[h3,h4]);
     const icons = createIcons(user.contacts);
   const divIcons = createElement('div',{classNames:['iconsSocialWrapper']},[...icons]);  
   
   const img = createCardImage(user);
   const article = createElement("article", { classNames: ["cardContainer"] }, [
     img,
-    h3,
-    h4,
+    nameWrapper,
     p,
     divIcons,
   ]);
